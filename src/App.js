aa2282b8ea9544acb41c58114dc6afcdb9f013bf
pradnya <pypradnya@gmail.com>
2024-09-21 22:11:58 +0530

    add folder stucture

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Accordion } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  const [activeKey, setActiveKey] = useState(null);
+
+  return (
+    <div className="App">
+      Celeb
+    </div>
+  );
+}
+
+export default App;
